1f81329a1b25474311d454801f52f45895bc6d24
ori winboim <yanuka7@gmail.com>
2022-09-20 11:56:50 +0300

    added alerts and loader to users

--- src/Pages/Users/Components/AddLifes.tsx
+++ src/Pages/Users/Components/AddLifes.tsx
@@ -12,13 +12,15 @@ import {
   Button,
 } from "@mui/material";
 import React, { useState } from "react";
-import { LifeFullType, LifeType } from "../../../Types/Types";
+import { AlertType, LifeFullType, LifeType } from "../../../Types/Types";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import dayjs, { Dayjs } from "dayjs";
 import { updateLifeDoc } from "../../../Database/database";
 import { useAuth } from "../../../Context/AdminContext";
+import Loader from "../../../Components/Loader";
+import Alerts from "../../../Components/Alerts";
 type Props = {
   lifes: LifeFullType;
 };
@@ -29,8 +31,14 @@ const AddLifes: React.FC<Props> = ({ lifes }) => {
   const [guestDate, setGuestDate] = useState<Dayjs | null>(dayjs());
   const [userSelect, setUserSelect] = useState(1);
   const [guestSelect, setGuestSelect] = useState(1);
-
+  const [isLoader, setIsLoader] = useState(false);
+  const [alertSettings, setAlertSettings] = useState<AlertType>({
+    isOpen: false,
+    type: "error",
+    message: "Error Loading Data...",
+  });
   const clickHandler = async () => {
+    setIsLoader(true);
     const obj = {
       user: {
         amount: userSelect,
@@ -43,97 +51,113 @@ const AddLifes: React.FC<Props> = ({ lifes }) => {
     };
     try {
       await updateLifeDoc(auth?.token!, obj);
-      console.log("Sucssfuly");
+      setAlertSettings({ isOpen: true, type: "success", message: "Updated Successfully" });
     } catch {
-      console.log("error");
+      setAlertSettings({
+        isOpen: true,
+        type: "error",
+        message: "Error Updating Data... Try Again Later",
+      });
     }
+    setIsLoader(false);
   };
 
   return (
-    <Paper>
-      <Box padding={2}>
-        <Typography variant="h6">Add Lifes To Users</Typography>
-        <Divider flexItem />
+    <>
+      <Alerts settings={alertSettings} setSettings={(val) => setAlertSettings(val)} />
+      <Loader isLoader={isLoader} />
+      <Paper>
+        <Box padding={2}>
+          <Typography variant="h6">Add Lifes To Users</Typography>
+          <Divider flexItem />
 
-        <Grid container p={3} width={"100%"} justifyContent={"space-around"}>
-          <Grid item xs={10} md={5}>
-            <Typography variant="subtitle2">Registered Users</Typography>
-            <Divider />
-            <Box display={'flex'} justifyContent={"center"}>
-              <Typography variant="subtitle1">
-                Default Amount : {lifes.registeredUserLife}
-              </Typography>
-              <FormControl>
-                <InputLabel id="userLifes">Lifes To Add</InputLabel>
-                <Select
-                  labelId="userLifes"
-                  // id="demo-simple-select"
-                  value={userSelect}
-                  label="Life To Add"
-                  onChange={(e: any) => setUserSelect(e.target.value)}
-                >
-                  {[...Array(9)].map((num, idx) => {
-                    return (
-                      <MenuItem key={idx} value={idx + 1}>
-                        {idx + 1}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DesktopDatePicker
-                  label="End Upgrade Date"
-                  inputFormat="DD/MM/YYYY"
-                  minDate={dayjs()}
-                  value={userDate}
-                  onChange={(e: Dayjs | null) => setUserDate(e)}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-            </Box>
-          </Grid>
+          <Grid container p={3} width={"100%"} justifyContent={"space-around"}>
+            <Grid item xs={10} md={5}>
+              <Typography variant="subtitle2">Registered Users</Typography>
+              <Divider sx={{ padding: "3px" }} />
+              <Box display={"flex"} flexDirection="column">
+                <Typography variant="subtitle1">
+                  Default Amount : {lifes.registeredUserLife}
+                </Typography>
+                <Box gap={2} display={"flex"} paddingTop={2} paddingBottom={2}>
+                  <FormControl>
+                    <InputLabel id="userLifes">Lifes To Add</InputLabel>
+                    <Select
+                      labelId="userLifes"
+                      // id="demo-simple-select"
+                      value={userSelect}
+                      label="Life To Add"
+                      onChange={(e: any) => setUserSelect(e.target.value)}
+                    >
+                      {[...Array(9)].map((num, idx) => {
+                        return (
+                          <MenuItem key={idx} value={idx + 1}>
+                            {idx + 1}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DesktopDatePicker
+                      label="End Upgrade Date"
+                      inputFormat="DD/MM/YYYY"
+                      minDate={dayjs()}
+                      value={userDate}
+                      onChange={(e: Dayjs | null) => setUserDate(e)}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </Box>
+              </Box>
+            </Grid>
 
-          <Grid item xs={10} md={5}>
-            <Typography variant="subtitle2">Guests Users</Typography>
-            <Divider />
-            <Box>
-              <Typography variant="subtitle1">Default Amount : {lifes.guestUserLife}</Typography>
-              <FormControl>
-                <InputLabel id="GuestLifes">Lifes To Add</InputLabel>
-                <Select
-                  labelId="GuestLifes"
-                  value={guestSelect}
-                  label="Life To Add"
-                  onChange={(e: any) => setGuestSelect(e.target.value)}
-                >
-                  {[...Array(9)].map((num, idx) => {
-                    return (
-                      <MenuItem key={idx} value={idx + 1}>
-                        {idx + 1}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DesktopDatePicker
-                  label="End Upgrade Date"
-                  inputFormat="DD/MM/YYYY"
-                  minDate={dayjs()}
-                  value={guestDate}
-                  onChange={(e: Dayjs | null) => setGuestDate(e)}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-            </Box>
+            <Grid item xs={10} md={5}>
+              <Typography variant="subtitle2">Guests Users</Typography>
+              <Divider sx={{ padding: "3px" }} />
+              <Box display={"flex"} flexDirection="column">
+                <Typography variant="subtitle1">Default Amount : {lifes.guestUserLife}</Typography>
+                <Box gap={2} display={"flex"} paddingTop={2} paddingBottom={2}>
+                  <FormControl>
+                    <InputLabel id="GuestLifes">Lifes To Add</InputLabel>
+                    <Select
+                      labelId="GuestLifes"
+                      value={guestSelect}
+                      label="Life To Add"
+                      onChange={(e: any) => setGuestSelect(e.target.value)}
+                    >
+                      {[...Array(9)].map((num, idx) => {
+                        return (
+                          <MenuItem key={idx} value={idx + 1}>
+                            {idx + 1}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DesktopDatePicker
+                      label="End Upgrade Date"
+                      inputFormat="DD/MM/YYYY"
+                      minDate={dayjs()}
+                      value={guestDate}
+                      onChange={(e: Dayjs | null) => setGuestDate(e)}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </Box>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
-        <Button variant="contained" onClick={clickHandler}>
-          Save
-        </Button>
-      </Box>
-    </Paper>
+          <Divider sx={{ margin: 2 }} />
+          <Box display={"flex"} width={"100%"} justifyContent={"center"}>
+            <Button variant="contained" onClick={clickHandler} sx={{ width: "20%" }}>
+              Save
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </>
   );
 };
 
--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -1,8 +1,10 @@
 import { Skeleton } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import Alerts from "../../Components/Alerts";
+import Loader from "../../Components/Loader";
 import { useAuth } from "../../Context/AdminContext";
 import { getAllUsers, getLifes } from "../../Database/database";
-import { LifeType, UserType } from "../../Types/Types";
+import { AlertType, LifeType, UserType } from "../../Types/Types";
 import AddLifes from "./Components/AddLifes";
 import UsersTable from "./Components/UsersTable";
 
@@ -16,25 +18,39 @@ const Users: React.FC<Props> = () => {
     getUsers();
     getLifeDoc();
   }, []);
+  const [alertSettings, setAlertSettings] = useState<AlertType>({
+    isOpen: false,
+    type: "error",
+    message: "Error Loading Data...",
+  });
 
   const getUsers = async () => {
-    const allUsers = await getAllUsers(auth?.token!);
-    setUsers(allUsers);
+    try {
+      const allUsers = await getAllUsers(auth?.token!);
+      setUsers(allUsers);
+    } catch {
+      setAlertSettings({ ...alertSettings, isOpen: true });
+    }
   };
   //1
   const getLifeDoc = async () => {
-    const lifeObj = await getLifes(auth?.token!);
-    setLifes(lifeObj[0]);
+    try {
+      const lifeObj = await getLifes(auth?.token!);
+      setLifes(lifeObj[0]);
+    } catch {
+      setAlertSettings({ ...alertSettings, isOpen: true });
+    }
   };
   return (
     <div>
+      <Alerts settings={alertSettings} setSettings={(val) => setAlertSettings(val)} />
       {users ? (
         <UsersTable users={users} setUsers={(val: any) => setUsers(val)} />
       ) : (
         <Skeleton height={300} animation={"wave"} />
       )}
       <br />
-      {lifes ? <AddLifes lifes={lifes} /> :  <Skeleton height={255} animation={"wave"} />}
+      {lifes ? <AddLifes lifes={lifes} /> : <Skeleton height={255} animation={"wave"} />}
     </div>
   );
 };
